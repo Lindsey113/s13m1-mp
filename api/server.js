e86ff0f4422100374bcc54e3deadbd64221d1120
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-17 12:29:47 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -5,26 +5,48 @@ const User = require('./users/model')
 
 server.get('/api/users', (req, res) => {
     User.find()
-    .then(users => {
-        res.json(users)
-    })
-    .catch(err => {
-        res.status(500).json({
-            err: err.message
+        .then(users => {
+            res.json(users)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                err: err.message
+            })
+        })
 })
 
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
-    .then(user => {
-        res.json(user)
-    })
-    .catch(err => {
-        res.status(404).json({
-            message: "The user with the specified ID does not exist"
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.json(user)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                err: err.message
+            })
+        })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    User.update(req.params.id)
+        .then(user => {
+            if(!user){
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            } else if(!user.name || !user.bio){
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                User.update(req.params.id, req.params.body)
+            }
+        })
 })
 
 

f2b985e81b2c25d00fe28ff0f970c4ef226d3c13
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-17 12:10:19 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,32 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            err: err.message
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    })
+})
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}

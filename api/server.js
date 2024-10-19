8fdf8dafe2bb71fdb6e8e50cb2e120efbf9ae14d
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-19 10:58:00 -0700

    donegit add .

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const server = express()
 const User = require('./users/model')
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -32,23 +33,78 @@ server.get('/api/users/:id', (req, res) => {
         })
 })
 
-server.put('/api/users/:id', (req, res) => {
-    User.update(req.params.id)
-        .then(user => {
-            if(!user){
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
+server.post('/api/users', (req, res) => {
+    const user = req.body
+
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+        User.insert(user)
+            .then(createdUser => {
+                res.status(201).json(createdUser)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the user to the database",
+                    error: err.message
                 })
-            } else if(!user.name || !user.bio){
+            })
+    }
+
+})
+
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if(!req.body.name || !req.body.bio){
                 res.status(400).json({
                     message: "Please provide name and bio for the user"
                 })
             } else {
-                User.update(req.params.id, req.params.body)
+                const updatedUser = await User.update(
+                    req.params.id,
+                    req.body
+                )
+                res.status(200).json(updatedUser)
             }
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message
         })
+    }
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            const deletedUser = await User.remove(
+                req.params.id,
+                req.body
+            )
+            res.status(200).json(deletedUser)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The user could not be removed",
+            err: err.message
+        })
+    }
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
